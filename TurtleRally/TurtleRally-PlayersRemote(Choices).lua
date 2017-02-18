ad1b53f497238f75bd8560fbf11cd6cfb00994eb
Niuttuc <hadri12pir@hotmail.fr>
2017-02-18 05:03:15 +0100

    Correction liste

--- TurtleRally/TurtleRally-PlayersRemote(Choices).lua
+++ TurtleRally/TurtleRally-PlayersRemote(Choices).lua
@@ -3,42 +3,42 @@
 --carte={
 --		nomListe=string,	Nom a afficher dans la liste<12 caracteres
 --		weight=number,		Nombre de cartes de ce type
---		message=string		Message modem à envoyer	
+--		message=string,		Message modem à envoyer	
 --
 --		}
 
 avance1={
-		nomListe='Avance 1'
+		nomListe='Avance 1',
 		weight=7,
-		message='forward 1'
+		message='forward 1',
 		}
 
 avance2={
-		nomListe='Avance 2'
+		nomListe='Avance 2',
 		weight=1,
-		message='forward 2'
+		message='forward 2',
 		}
 
 clockTurn={
-		nomListe='Tour droite'
+		nomListe='Tour droite',
 		weight=8,
-		message='turnRight 1'
+		message='turnRight 1',
 		}
 
 trigoTurn={
-		nomListe='Tour gauche'
+		nomListe='Tour gauche',
 		weight=8,
-		message='turnLeft 1'
+		message='turnLeft 1',
 		}
 
 turnBack={
-		nomListe='Demi-tour'
+		nomListe='Demi-tour',
 		weight=8,
-		message='turnLeft 2'
+		message='turnLeft 2',
 		}
 
 backUp={
-		nomListe='Recule 1'
+		nomListe='Recule 1',
 		weight=8,
-		message='goBack 1'
+		message='goBack 1',
 		}
