98386ff7ff7f5ae650f618fc217f3d33fcb70aad
Niuttuc <hadri12pir@hotmail.fr>
2017-02-18 04:47:13 +0100

    Créations des choix possibles
    Ajout des choix possibles.

---
+++ TurtleRally/TurtleRally-PlayersRemote(Choices).lua
@@ -0,0 +1,44 @@
+
+
+--carte={
+--		nomListe=string,	Nom a afficher dans la liste<12 caracteres
+--		weight=number,		Nombre de cartes de ce type
+--		message=string		Message modem à envoyer	
+--
+--		}
+
+avance1={
+		nomListe='Avance 1'
+		weight=7,
+		message='forward 1'
+		}
+
+avance2={
+		nomListe='Avance 2'
+		weight=1,
+		message='forward 2'
+		}
+
+clockTurn={
+		nomListe='Tour droite'
+		weight=8,
+		message='turnRight 1'
+		}
+
+trigoTurn={
+		nomListe='Tour gauche'
+		weight=8,
+		message='turnLeft 1'
+		}
+
+turnBack={
+		nomListe='Demi-tour'
+		weight=8,
+		message='turnLeft 2'
+		}
+
+backUp={
+		nomListe='Recule 1'
+		weight=8,
+		message='goBack 1'
+		}
--- TurtleRally/TurtleRally-PlayersRemote.lua
+++ TurtleRally/TurtleRally-PlayersRemote.lua
@@ -4,6 +4,9 @@ color=colors[colorString]
 
 os.loadAPI('ahb')
 os.loadAPI('windows')
-
-
+os.loadAPI('choices')
+totalWeight=0
+for i=1,#choices do
+	totalWeight=totalWeight+choices[i].weight
+end
 
