1da79d0a04bcfe7dbe9d1b924dbe34ecb8d36398
Niuttuc <hadri12pir@hotmail.fr>
2017-06-23 21:45:00 +0200

    Problèmes? Aucun problème!

--- Quae/CommandComputer/CommandAPI.lua
+++ Quae/CommandComputer/CommandAPI.lua
@@ -4,22 +4,22 @@ chat=peripheral.find("chatBox")
 function stop()
 	error('Arret utilisateur')
 end
-commands={["stop"]={["group"]="_OPS_",["func"]=stop }}
+storedCommands={["stop"]={["group"]="_OPS_",["func"]=stop }}
 
 function addCommand(command,group,func)
-	commands[command]={["group"]=group,["func"]=func}
+	storedCommands[command]={["group"]=group,["func"]=func}
 end
 
 function waitCommands()
 	while true do
 		ev,player,args=os.pullEvent("command")
-		if commands[args[1]] then
+		if storedCommands[args[1]] then
 			command=args[1]
 			table.remove(args,1)
 			whatever,playerGroups=commands.p('user',player)
 			for i=2,#playerGroups do
-				if string.sub(playerGroups[i],3,-1)==commands[args[1]].group then
-					commands[args[1]].func()
+				if string.sub(playerGroups[i],3,-1)==storedCommands[command].group then
+					storedCommands[command].func()
 				end
 			end
 		end
