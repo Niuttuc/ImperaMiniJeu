9b5d73e370668a80d0be15ae872e176cd7cb2338
Mandonnaud <mandonnaud.s@gmail.com>
2017-10-16 14:22:43 +0200

    oh ?

--- cacheCache/startup.lua
+++ cacheCache/startup.lua
@@ -1,6 +1,6 @@
 -- V1.01
 commands.exec("/effect Adsl_Houba 14 6000 1 true")
 while true do
-	commands.exec("/execute Adsl_Houba ~ ~ ~ /summon falling_block ~ ~ ~  {TileID:75}")
-	-- falling_block n'existe pas :/
+	commands.exec("/execute Adsl_Houba /summon FallingSand ~ ~0.5 ~ {Tile:1}")
+	-- falling_block
 end
